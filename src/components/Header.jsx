30f68b98716ae5b389a699fd6c6248dec8589598
akosinoan <noan.babao@gmail.com>
2024-03-01 22:31:20 -0600

    changed profilepic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () =>{
                 <div className=" relative z-30 pt-20 pb-12 sm:pt-56 sm:pb-48 lg:pt-64 lg:pb-48">
                     <div className="flex flex-col items-center justify-center lg:flex-row">
                         <div className="rounded-full border-8 border-primary shadow-xl">
-                        <img src="./profpic.jpg" className=" h-48 w-48 rounded-full sm:h-48" alt="author" />
+                        <img src="./profpic2.jpg" className=" h-48 w-48 rounded-full sm:h-48" alt="author" />
                         </div>
                         <div className="pt-8 sm:pt-10 lg:pl-8 lg:pt-0">
                         <h1 className="text-center font-serif text-4xl text-white sm:text-left sm:text-5xl md:text-6xl">
