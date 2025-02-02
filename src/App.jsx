0463299bdc5af6e02ee674f8e0660cf668fc13a0
noan babao <noan.babao@gmail.com>
2025-02-02 16:48:33 -0600

    update package.json

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,17 @@ const App =() =>{
   const [activeNav , setActiveNav]=useState('Home');
     
   return (
-    <div className='max-h flex flex-col'>
-      <ActiveNavContext.Provider value={[activeNav , setActiveNav]}  >
-        <Header />
-        <MainComponent />
-        <Footer />
+    <div> TEST </div>
+    // <div className='max-h flex flex-col'>
+    //   <ActiveNavContext.Provider value={[activeNav , setActiveNav]}  >
+    //     <Header />
+    //     <MainComponent />
+    //     <Footer />
         
         
-      </ActiveNavContext.Provider>
+    //   </ActiveNavContext.Provider>
       
-    </div>
+    // </div>
   )
 }
 
