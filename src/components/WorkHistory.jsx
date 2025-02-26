7368b73cb198773bd59f77a5526581a3746e71da
noan babao <noan.babao@gmail.com>
2025-02-25 20:22:59 -0600

    added work experience icons

--- src/components/WorkHistory.jsx
+++ src/components/WorkHistory.jsx
@@ -11,10 +11,10 @@ const WorkHistory = () =>{
     
     //Work Experience Data
     const workExperiences = [
-        {name:"SSE", position:"Subject Matter Expert" ,date:"Jan 2021 - Apr 2024", duration:"3 yrs 4 mos", desc: "Description"},
-        {name:"US Navy", position:"Aviation Structural Mechanic" ,date:"Dec 2015 - Dec 2019", duration:"4 yrs", desc: "Description"},
-        {name:"Intevalue Services Inc.", position:"Java Web Developer" ,date:"2012 - 2015 ", duration:"3 yrs", desc: "Description"},
-        {name:"Manila Cordage Company", position:"PHP Web Developer (Intern)" ,date:"May 2011 - Jul 2011 ", duration:"3 mos", desc: "Description"},
+        {name:"SSE", position:"Subject Matter Expert" ,date:"2021 - 2024", duration:"3 yrs 4 mos", desc: "Description" , imgSrc:"./sse_logo.jpeg"},
+        {name:"US Navy", position:"Aviation Structural Mechanic" ,date:"2015 - 2019", duration:"4 yrs", desc: "Description",imgSrc:"./us_navy_logo.jpeg"},
+        {name:"Intevalue Services Inc.", position:"Java Web Developer" ,date:"2012 - 2015 ", duration:"3 yrs", desc: "Description", imgSrc:"intevalue.jpeg"},
+        {name:"Manila Cordage Company", position:"PHP Web Developer (Intern)" ,date:"2011", duration:"3 mos", desc: "Description"},
         
     ];
 
@@ -22,16 +22,16 @@ const WorkHistory = () =>{
         <section className="bg-gray-200 scroll-mt-20 pt-20" id="workhistory" >
             
             
-                <div ref={ref1} className={  ` text-center relative  ${(isVisible1 ? "animate-fadeLeftToRight" : "opacity-0")} `}>
+                <div ref={ref1} className={  `text-center relative  ${(isVisible1 ? "animate-fadeLeftToRight" : "opacity-0")} `}>
                     <h2 className="text-red-900 text-4xl font-semibold uppercase text-primary sm:text-5xl lg:text-6xl">
-                        My Work Experience
+                        Work Experience
                     </h2>
                 </div>
             
             <div ref={ref2} className={`relative ${(isVisible2 ? "animate-fadeLeftToRight" : "opacity-0")} `} >
                 <ol  className={` lg:list-none list-disc lg:grid lg:grid-cols-2 sm:flex sm:flex-col px-10  py-16 sm:py-20 items-center`}>  
                     
-                {workExperiences.map((workExperience,index) => <WorkExperience key={index} index={index} name={workExperience.name} position={workExperience.position} date={workExperience.date} desc={workExperience.desc }   duration={workExperience.duration} />)}
+                {workExperiences.map((workExperience,index) => <WorkExperience key={index} imgSrc={workExperience.imgSrc} index={index} name={workExperience.name} position={workExperience.position} date={workExperience.date} desc={workExperience.desc }   duration={workExperience.duration} />)}
                 
             
                     {/* <li ref={ref2} className={ `lg:w-[40%] lg:mt-28 text-center  lg:text-leftrelative  ${(isVisible2 ? "animate-fadeLeftToRight" : "opacity-0")} `}>
@@ -64,29 +64,51 @@ const WorkHistory = () =>{
 const WorkExperience = (props) =>{
    
     const isEven = props.index % 2 == 0;
+    
     return (    
-        <li className={ `px-10 border-red-900 lg:w-auto sm:w-1/2  border-l-4  text-left ${isEven ? 'lg:border-r-0 lg:text-right lg:border-0' : 'lg:border-l-4'} `}>
-            <h4 className={`pt-6 ${isEven ? '' : 'lg:mt-56'} font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl`}>
-                {props.logo || 'logo'} 
-            </h4>
+        <>
+        <li className={ `border-red-900 lg:w-auto w-96  lg:bg-inherit border-l-4  text-left ${isEven ? 'lg:border-r-0 lg:text-right lg:border-0 bg-gray-100' : 'lg:border-l-4 '} `}>
+            <div className={`lg:flex lg:flex-row`}>
             
-            <h4 className={`pt-0 font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl`}>
-                {props.name}
-            </h4>
-            <h4 className={`pt-0 font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl`}>
-                {props.position}
-            </h4>
-            <h3 className={` pt-0 font-header text-xl font-medium text-gray-400 sm:text-2xl lg:text-3xl`}>
-                {props.date}
-            </h3>
-            <h3 className={` pt-0 font-header text-xl font-medium text-gray-400 sm:text-2xl lg:text-3xl`}>
-                {`(${props.duration})`}
-            </h3>
-            <p className=" pt-6 px-10 font-body leading-relaxed text-gray-400 ">
-                {props.desc}
-            </p>
+            {!isEven ? <div className={"hidden lg:flex pt-10 mt-56 text-4xl italic font-extrabold text-red-900 "}> {`<<`} </div>  :''}
+                
+                <div className={`mx-4 w-full`}>
+                    <div className={`lg:flex ${isEven ? ' lg:flex-row-reverse ' : ' lg:mt-56'} `}>
+                        
+                        <h4 className={`pt-6  font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl`}>
+                            <span className={`relative h-20 w-20`}> 
+                                <img src={props.imgSrc || `noanworks.svg`} className= {`h-20 w-20 rounded-full sm:h-20 ${!props.imgSrc? 'opacity-10 ':''}`}  alt={ `logo` } /> 
+                                {!props.imgSrc ? <div className={`absolute left-1 top-8 text-xs bg-white`} > Not available </div> : ''}
+                            </span>
+                        </h4>
+                        
+                        <div className={`lg:px-1 lg:pt-8`}>
+                            <h4 className={`pt-0 font-header text-xl font-medium text-red-900 sm:text-2xl lg:text-3xl`}>
+                                {props.name}
+                            </h4>
+                            
+                            <h4 className={`italic pt-0 font-header text-sm font-medium text-red-900 leading-relaxed`}>
+                                {props.position}
+                            </h4>
+                        </div>
+                        
+                    </div>
+                 
+                    <h3 className={` pt-0 font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl`}>
+                            {props.date}
+                        </h3>
+                        <h3 className={` pt-0 font-header font-medium text-gray-400 leading-relaxed`}>
+                            {`(${props.duration})`}
+                        </h3>
+                    <p className=" pt-6 px-10 font-body leading-relaxed text-gray-400 ">
+                        {props.desc}
+                    </p>
+                </div>
+                {isEven ?  <div className={`hidden lg:flex px-2 mt-10 text-right w-14 text-4xl italic font-extrabold text-red-900`}> {`>>`}</div> : ''}
+            </div>
         </li> 
-
+        
+        </>
     )
 }
 
